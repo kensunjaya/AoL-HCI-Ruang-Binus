332a37423e273f7fa9f4c6181256b39344bbe821
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-06-06 20:52:04 +0700

    added browser router

---
+++ src/pages/videolearning.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const VideoLearning = () =>{
+  return (
+    <div className="min-h-screen w-screen bg-gradient-to-r from-orange-100 to-slate-400">
+      <Navbar />
+      <h1 className="text-reds text-10xl font-bold">Video Based Learning</h1>
+    </div>
+  );
+}
+
+export default VideoLearning
