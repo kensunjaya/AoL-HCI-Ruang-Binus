54cbc7216dbe82513e302c566015acd7595b7d5c
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-06-19 00:52:53 +0700

    fixed several stylings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,15 +9,15 @@ import { AuthContext } from "../context/AuthContext"
 const Navbar = (props: any) => {
   const user = useContext(AuthContext);
   return (
-    <div className="flex">
+    <div className="flex whitespace-nowrap">
       <div className="flex items-center">
         <img src={logo} alt="Logo" className="w-50 h-20"/>
-        <CustomButton title="Home" onClick="/" style={`shadow-lg font-sans font-semibold m-5 ${props.active === "home" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
-        <CustomButton title="Forum" onClick="/forum" style={`shadow-lg font-sans font-semibold m-5 ${props.active === "forum" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
-        <CustomButton title="Material" onClick="/material" style={`shadow-lg font-sans font-semibold m-5 ${props.active === "material" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
-        <CustomButton title="Video Learning" onClick="/videolearning" style={`shadow-lg font-sans font-semibold m-5 ${props.active === "videolearning" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
-        <CustomButton title="Exercise" onClick="/exercise" style={`shadow-lg font-sans font-semibold m-5 ${props.active === "exercise" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
-        <CustomButton title="AI Chatbot" onClick="/chatbot" style={`shadow-lg font-sans font-semibold m-5 ${props.active === "chatbot" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
+        <CustomButton title="Home" onClick="/" style={`shadow-lg font-sans font-semibold m-[1rem] ${props.active === "home" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
+        <CustomButton title="Forum" onClick="/forum" style={`shadow-lg font-sans font-semibold m-[1rem] ${props.active === "forum" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
+        <CustomButton title="Material" onClick="/material" style={`shadow-lg font-sans font-semibold m-[1rem] ${props.active === "material" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
+        <CustomButton title="Video Learning" onClick="/videolearning" style={`shadow-lg font-sans font-semibold m-[1rem] ${props.active === "videolearning" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
+        <CustomButton title="Exercise" onClick="/exercise" style={`shadow-lg font-sans font-semibold m-[1rem] ${props.active === "exercise" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
+        <CustomButton title="AI Chatbot" onClick="/chatbot" style={`shadow-lg font-sans font-semibold m-[1rem] ${props.active === "chatbot" ? "bg-blue-400 text-white" : "text-blue-400 bg-white hover:bg-blue-100"}`}/>
       </div>
       <div className="flex items-center justify-end flex-grow pr-[1vh] font-sans font-semibold">
         <Link to="/profile" className="hover:bg-slate-400 p-3 rounded-full">
